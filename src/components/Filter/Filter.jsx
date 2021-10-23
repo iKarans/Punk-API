740d4ae1b9cbb6cbc7e14abfe1724aa47fe5449a
iKarans <68602829+iKarans@users.noreply.github.com>
2021-10-23 15:17:25 +0100

    added voutput to show value of slider

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -13,8 +13,10 @@ const Filter = (props) => {
             <div className="filter">
                 <label htmlFor="pH">pH:</label>
                 <input type="range" min="3" max="5" step="0.1" value={phLevel} className="slider" id="pH" onInput={handlePhChange} />
+                <output name="result" for="pH">{phLevel == 3 ? "3 < pH <= 5" : phLevel}</output>
                 <label htmlFor="ABV">ABV:</label>
                 <input type="range" min="0" max="15" value={abvLevel} className="slider" id="ABV" onInput={handleAbvChange}/>
+                <output name="result" for="ABV">{abvLevel == 0 ? 0 : abvLevel - 1}{" < ABV <= "}{abvLevel == 0 ? 15 : abvLevel}</output>
                 <label htmlFor="scales">Classic:</label>
                 <input type="checkbox" id="classic" name="classic" onInput={handleIsClassic}/>
             </div>
