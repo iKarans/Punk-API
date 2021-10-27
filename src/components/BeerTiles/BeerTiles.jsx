ea8f61306c0ee8bf97d8c24a802af106a43c4349
iKarans <68602829+iKarans@users.noreply.github.com>
2021-10-27 13:39:59 +0100

    firebase workflow

--- src/components/BeerTiles/BeerTiles.jsx
+++ src/components/BeerTiles/BeerTiles.jsx
@@ -8,7 +8,7 @@ const BeerTiles = (props) => {
         <section className="beer-tiles">
             {beers && (
                 beers.filter((beer) => {
-                    return (isClassic ? parseInt(beer.first_brewed.slice(3)) < 2010 : true) && beer.name.toLowerCase().includes(searchWord.toLowerCase()) && beer.image_url && (phLevel == 3 ? true : beer.ph == phLevel) && (abvLevel == 0 ? true : beer.abv <= abvLevel && beer.abv > abvLevel - 1)
+                    return (isClassic ? parseInt(beer.first_brewed.slice(3)) < 2010 : true) && beer.name.toLowerCase().includes(searchWord.toLowerCase()) && beer.image_url && (phLevel === 3 ? true : beer.ph === phLevel) && (abvLevel === 0 ? true : beer.abv <= abvLevel && beer.abv > abvLevel - 1)
                 }).map((beer, index) => {
                     return (
                         <Beer image_url={beer.image_url} name={beer.name} tagline={beer.tagline} description={beer.description} abv={beer.abv} ph={beer.ph} addToFavourites={addToFavourites} firebaseFavourites={firebaseFavourites} deleteFavourites={deleteFavourites} key={beer.name + index} />
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -4,19 +4,19 @@ import "./Filter.scss";
 const Filter = (props) => {
     const { handleIsClassic, phLevel, handlePhLevel, abvLevel, handleAbvLevel } = props;
     const handlePhChange = (event) => {
-        handlePhLevel(event.target.value);
+        handlePhLevel(parseInt(event.target.value));
     }
     const handleAbvChange = (event) => {
-        handleAbvLevel(event.target.value)
+        handleAbvLevel(parseInt(event.target.value))
     }
     return (
             <div className="filter">
                 <label htmlFor="pH">pH:</label>
                 <input type="range" min="3" max="5" step="0.1" value={phLevel} className="slider" id="pH" onInput={handlePhChange} />
-                <output name="result" for="pH">{phLevel == 3 ? "3 < pH <= 5" : phLevel}</output>
+                <output name="result" for="pH">{phLevel === 3 ? "3 < pH <= 5" : phLevel}</output>
                 <label htmlFor="ABV">ABV:</label>
                 <input type="range" min="0" max="15" value={abvLevel} className="slider" id="ABV" onInput={handleAbvChange}/>
-                <output name="result" for="ABV">{abvLevel == 0 ? 0 : abvLevel - 1}{" < ABV <= "}{abvLevel == 0 ? 15 : abvLevel}</output>
+                <output name="result" for="ABV">{abvLevel === 0 ? 0 : abvLevel - 1}{" < ABV <= "}{abvLevel === 0 ? 15 : abvLevel}</output>
                 <label htmlFor="scales">Classic:</label>
                 <input type="checkbox" id="classic" name="classic" onInput={handleIsClassic}/>
             </div>
